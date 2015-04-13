47eaefa2bc2682e5dbecd266b47f0d58507f8db2
Alex Mejias <contact@lazers.tv>
2015-04-13 10:17:54 -0400

    v0

---
+++ app/Stream.php
@@ -0,0 +1,33 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Session;
+
+class Stream extends Model {
+
+    /**
+     * @var string
+     */
+	public $token = '';
+
+    /**
+     * Sets up song token for one time use.
+     *
+     * @param $token
+     */
+    public function setToken($token)
+    {
+        Session::flash('songToken', $token);
+        return($token);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return(Session::get('songToken'));
+    }
+
+
+}
