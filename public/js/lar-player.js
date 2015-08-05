97cfe77abe58156d8238d405224d51af47947fd6
karatechops <contact@lazers.tv>
2015-08-05 18:02:46 -0400

    v0

--- public/js/lar-player.js
+++ public/js/lar-player.js
@@ -233,7 +233,8 @@ Player.prototype = {
             url: '/streamsong/'+song.id+'/',
             cache: false,
             success: function(data) {
-                var stream = '/streamsong/'+song.id+'/'+data+'?11';
+                var rando = Math.random() * (999 - 100) + 100;
+                var stream = '/streamsong/'+song.id+'/'+data+'?'+rando;
                 console.log('Data: '+data+' Status: '+status);
                 Player.sendSongToPlayer(song, playSongNow, previousSong, stream);
             },
