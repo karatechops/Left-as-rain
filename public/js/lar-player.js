71d4fadf3139f1a33394ad12fc0a4a80c5c46f22
karatechops <contact@lazers.tv>
2015-08-05 17:21:27 -0400

    v0

--- public/js/lar-player.js
+++ public/js/lar-player.js
@@ -1,9 +1,9 @@
-$.ajaxSetup({
+/*$.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
-
+*/
 function Playlist (playlistDiv)
 {
     this.playlistDiv = playlistDiv;
