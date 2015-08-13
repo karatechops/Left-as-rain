c56c38352a64a82f0c4d2d29826b70ac5930fbd7
karatechops <contact@lazers.tv>
2015-08-12 20:05:46 -0400

    v0

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-	<title>Left as rain | Impossibly Chill</title>
+    <meta property="fb:admins" content="39601694"/>
+    <meta property="fb:app_id" content="382016265209375"/>
+    <meta property="og:image" content="http://leftasrain.com/img/lar_200.jpg"/>
+    <meta property="og:title" content="left as rain / an alright music blog"/>
+    <meta property="og:type" content="blog"/>
+    <meta property="og:description" content="A place where Chris Thomas and Alex Mejias share their feelings while playing marginally okay music."/>
+    <meta property="og:url" content="http://www.leftasrain.com/"/>
+    <meta name="description" content="A place where Chris Thomas and Alex Mejias share their feelings while playing marginally okay music."/>
+
+    <title>Left as rain | Impossibly Chill</title>
 
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
 
