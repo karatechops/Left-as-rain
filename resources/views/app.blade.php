71f36b95420c9d51f97e1109d4f01622c00e6093
karatechops <contact@lazers.tv>
2015-08-12 20:02:25 -0400

    v0

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 	<title>Left as rain | Impossibly Chill</title>
 
-    <script src="{{ asset('js/modernizr.min.js') }}"></script>
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
 
 </head>
@@ -22,6 +21,7 @@
     <div class="clearfix"></div>
     <div class="shading"></div>
 
+    <script src="{{ asset('js/modernizr.min.js') }}"></script>
 	<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
