81ef937cceaa453d26587bd38f4bcabe47bc04c7
karatechops <contact@lazers.tv>
2015-08-14 09:34:34 -0400

    v0

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -18,9 +18,7 @@ class StreamController extends Controller {
 
         if ($token == null) {
            if (Request::ajax()) {
-                $sessionToken = str_random(40);
-                Session::put('songToken', $sessionToken);
-                //$token = Stream::setToken(str_random(40));
+                $sessionToken = $stream->setToken(str_random(40));
                 return response($sessionToken);
             } else {
                 return 'no way jose';
@@ -28,29 +26,7 @@ class StreamController extends Controller {
         }
 
         if ($token ==  Session::get('songToken') ) {
-            //return($stream->sendStream($id));
-            $post = Post::find($id);
-            $pathToFile = base_path().'/storage/app/mp3/'.$post->song_path;
-            //$pathToFile = base_path().'/storage/app/mp3/holy-soul.mp3';
-            $fileSize = filesize($pathToFile);
-            $size   = filesize($pathToFile);
-            $length = $size;
-            $start  = 0;
-            $end    = $size - 1;
-            $name = $post->song_path;
-            $headers = array(
-                'Content-Type'=>'audio/mpeg',
-                'Pragma'=>'public',
-                'Content-Transfer-Encoding' => 'binary',
-                'Expires'=> 0,
-                'Cache-Control'=> 'must-revalidate, post-check=0, pre-check=0',
-                'Filename'=>$name,
-                'Content-Length'=>$length,
-                'Connection'=> 'keep-alive'
-            );
-
-            //'Content-Range'=>'bytes '.$start.'-'.$end.'/'.$size,
-            return response()->download($pathToFile, $name, $headers);
+            return($stream->sendStream($id));
         }
     }
 
--- app/Stream.php
+++ app/Stream.php
@@ -44,14 +44,22 @@ class Stream extends Model {
         //$pathToFile = base_path().'/storage/app/mp3/'.$post->song_path;
         $pathToFile = base_path().'/storage/app/mp3/holy-soul.mp3';
         $fileSize = filesize($pathToFile);
+        $size   = filesize($pathToFile);
+        $length = $size;
+        $start  = 0;
+        $end    = $size - 1;
         $name = $post->song_path;
         $headers = array(
             'Content-Type'=>'audio/mpeg',
             'Pragma'=>'public',
             'Content-Transfer-Encoding' => 'binary',
+            'Accept-Ranges' => 'bytes',
+            'Accept-Encoding' => 'gzip,deflate,sdch',
             'Expires'=> 0,
             'Cache-Control'=> 'must-revalidate, post-check=0, pre-check=0',
             'Filename'=>$name,
+            'Content-Length'=>$length,
+            'Content-Range'=>'bytes '.$start.'-'.$end.'/'.$size,
             'Connection'=> 'keep-alive'
         );
 
