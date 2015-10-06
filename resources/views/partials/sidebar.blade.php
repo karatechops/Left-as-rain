bf3755a9c043c72af0bd9aadc8e842fbb96d5d4a
karatechops <contact@lazers.tv>
2015-10-05 21:14:59 -0400

    v0

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,16 @@
 <div class="side-bar">
     <div class="side-bar-content">
-        <img src="/img/banner-test.jpg">
+        <div class="feature">
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- LAR 300x250 -->
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:300px;height:250px"
+                 data-ad-client="ca-pub-6310318869975746"
+                 data-ad-slot="9560892118"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
         <p>
             <a href="/pages/terms-and-conditions">terms and conditions</a>
             <br>
